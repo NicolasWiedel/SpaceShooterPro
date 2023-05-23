b8c90ef93fa9765dd1dc399ccbad656e0744d917
nicolaswiedel <nicolaswiedel@googlemail.com>
2023-05-23 18:02:40 +0200

    12.98.

--- SpaceShooterPro/Assets/Scripts/UIManager.cs
+++ SpaceShooterPro/Assets/Scripts/UIManager.cs
@@ -11,11 +11,17 @@ public class UIManager : MonoBehaviour
     private Image _livesImage;
     [SerializeField]
     private Sprite[] _sprites;
+    [SerializeField]
+    private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _restartText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -26,5 +32,28 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImage.sprite = _sprites[currentLives];
+
+        if(currentLives == 0)
+        {
+            GameOverSequence();
+        }
+    }
+
+    private void GameOverSequence()
+    {
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
